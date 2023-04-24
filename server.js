f0c7bddef27ad45008701ada5b7ae04905ebdc1b
Anne <steamengine.a@gmail.com>
2023-04-24 19:17:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const MongoClient = require("mongodb").MongoClient;
-MongoClient.connect(
-  "mongodb+srv://admin:qwer1234@cluster0.6juaf4r.mongodb.net/?retryWrites=true&w=majority"
-);
 
-app.listen("8080", function () {
-  console.log("listening on 8080");
-});
+var db;
+
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.6juaf4r.mongodb.net/?retryWrites=true&w=majority",
+  function (err, client) {
+    if (err) {
+      return console.log(err);
+    }
+
+    db = client.db("todoapp");
+
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+    });
+  }
+);
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -25,4 +35,10 @@ app.post("/add", function (req, res) {
   //console.log(req.body);
   console.log(req.body.title);
   console.log(req.body.date);
+  db.collection("post").insertOne(
+    { Title: req.body.title, Date: req.body.date, _id: 1 },
+    function (err, result) {
+      console.log("Saved");
+    }
+  );
 });
